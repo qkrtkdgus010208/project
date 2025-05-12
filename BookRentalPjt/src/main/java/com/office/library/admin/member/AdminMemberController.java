418f250c8daa1c824900222194c22daf472c9a9b
qkrtkdgus010208 <psh010208@naver.com>
2025-05-12 22:17:30 +0900

    11주차 1차시
    이번 장에서 진행할 내용
    – 웹 서비스 구현을 위한 프로젝트의 전체 흐름과 구조 이해
    – 프로젝트 생성 및 필요한 파일 준비
    – 관리자 회원 가입 기능 구현
    – 데이터베이스 연동 설정 및 연동 방법 학습
    – 데이터 암호화 방법 학습

---
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -0,0 +1,38 @@
+package com.office.library.admin.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin/member")
+public class AdminMemberController {
+	
+	@Autowired
+	AdminMemberService adminMemberService;
+	
+	@RequestMapping(value = "/createAccountForm", method = RequestMethod.GET)
+	public String createAccountForm() {
+		System.out.println("[AdminMemberController] createAccountForm()");
+		
+		String nextPage = "admin/member/create_account_form";
+		
+		return nextPage;
+	}
+	
+	@PostMapping("/createAccountConfirm")
+	public String createAccountConfirm(AdminMemberVo adminMemberVo) {
+		System.out.println("[AdminMemberController] createAccountConfirm()");
+		
+		String nextPage = "admin/member/create_account_ok";
+		
+		int result = adminMemberService.createAccountConfirm(adminMemberVo);
+		
+		if (result <= 0)
+			nextPage = "admin/member/create_account_ng";
+		
+		return nextPage;
+	}
+}
