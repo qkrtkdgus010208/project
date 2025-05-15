e16f7ee8b4a40ce81142ad71f51990df9600224a
qkrtkdgus010208 <psh010208@naver.com>
2025-05-15 18:43:51 +0900

    11주차 2차시
    ■ 관리자 로그인 화면을 구현
    ■ 관리자 로그인 인증을 처리하기 위한 컨트롤러, 서비스, Dao 구현
    ■ 쿠키를 이용한 로그인 상태 유지 및 로그아웃 기능 구현
    ■ 세션을 이용한 로그인 상태 유지 및 로그아웃 기능 구현
    ■ 일반 관리자를 로그인할 수 있도록 승인
    ■ 최고 관리자와 일반 관리자를 구분하여 메뉴를 구성

--- BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -1,10 +1,17 @@
 package com.office.library.admin.member;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin/member")
@@ -32,6 +39,68 @@ public class AdminMemberController {
 		
 		if (result <= 0)
 			nextPage = "admin/member/create_account_ng";
+		
+		return nextPage;
+	}
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		System.out.println("[AdminMemberController] loginForm()");
+		
+		String nextPage = "admin/member/login_form";
+		
+		return nextPage;
+	}
+	
+	@PostMapping("/loginConfirm")
+	public String loginConfirm(AdminMemberVo adminMemberVo, HttpSession session) {
+		System.out.println("[AdminMemberController] loginConfirm()");
+		
+		String nextPage = "admin/member/login_ok";
+		
+		AdminMemberVo loginedAdminMemberVo = adminMemberService.loginConfirm(adminMemberVo);
+		
+		if (loginedAdminMemberVo == null) {
+			nextPage = "admin/member/login_ng";
+		}else {
+			session.setAttribute("loginedAdminMemberVo", loginedAdminMemberVo);
+			session.setMaxInactiveInterval(60 * 30);
+		}
+		
+		return nextPage;
+	}
+	
+	@GetMapping("/logoutConfirm")
+	public String logoutConfirm(HttpSession session) {
+		System.out.println("[AdminMemberController] logoutConfirm()");
+		
+		String nextPage = "redirect:/admin";
+		
+		session.invalidate();
+		
+		return nextPage;
+	}
+	
+	@GetMapping("/listupAdmin")
+	public String listupAdmin(Model model) {
+		System.out.println("[AdminMemberController] modifyAccountConfirm()");
+		
+		String nextPage = "admin/member/listup_admins";
+		
+		List<AdminMemberVo> adminMemberVos = adminMemberService.listupAdmin();
+		
+		model.addAttribute("adminMemberVos", adminMemberVos);
+		
+		return nextPage;
+	}
+	
+	@GetMapping("/setAdminApproval")
+	public String setAdminApproval(@RequestParam("a_m_no") int a_m_no) {
+		System.out.println("[AdminMemberController] setAdminApproval()");
+		
+		String nextPage = "redirect:/admin/member/listupAdmin";
+		
+		adminMemberService.setAdminApproval(a_m_no);
 		
 		return nextPage;
 	}
